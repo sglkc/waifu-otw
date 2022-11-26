f5f92a9c40b6d80ae2f99f9b2ececd1190179e89
sglkc <31957516+sglkc@users.noreply.github.com>
2022-11-26 15:37:15 +0700

    feat: shizuku reply to sent message
    chore: compress assets
    style: minor styling adjustment

--- script/script.js
+++ script/script.js
@@ -1,3 +1,36 @@
-console.log('nganu')
+const form = document.getElementById('form');
+const input = document.getElementById('message');
+const messages = document.getElementById('messages');
 
-nlp.process('nganu').then(console.log);
+const createMessage = (sender, message) => {
+  const div = document.createElement('div');
+
+  div.className = sender;
+  div.innerText = message;
+
+  messages.append(div);
+  div.scrollIntoView();
+}
+
+const processMessage = (message) => {
+  // random delay for "authenticity"
+  const delay = Math.random() * 2000 + 300;
+  nlp
+    .process(message)
+    .then((e) =>
+      setTimeout(() => createMessage('shizuku', e.answer), delay)
+    );
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const message = input.value.trim();
+
+  if (!message.length) return;
+
+  createMessage('me', message);
+  processMessage(message);
+
+  input.value = '';
+});
