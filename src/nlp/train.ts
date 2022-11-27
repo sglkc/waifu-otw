07502f2e1778e35b205cd8ced1ba3192c788763f
sglkc <31957516+sglkc@users.noreply.github.com>
2022-11-27 13:53:38 +0700

    feat: script for nlp training
    build: add ts-node
    build: remove unused pixi.js
    build: bump @pixi deps to version ^6.5
    chore: remove unused file

--- src/nlp/train.ts
+++ src/nlp/train.ts
@@ -1,4 +1,6 @@
+// @ts-ignore
 const { writeFileSync } = require('fs');
+const { join } = require('path');
 const { containerBootstrap } = require('@nlpjs/core');
 const { Nlp } = require('@nlpjs/nlp');
 const { LangEn } = require('@nlpjs/lang-en');
@@ -10,32 +12,44 @@ const corpora = [
   require('./corpus-ja.json')
 ];
 
+interface corpus {
+  locale: string;
+  data: Array<data>;
+};
+
+interface data {
+  intent: string;
+  utterances: Array<string>;
+  answers: Array<string>;
+}
+
 (async () => {
   const container = await containerBootstrap();
 
   container.use(Nlp);
   container.use(LangEn);
   container.use(LangId);
+  container.use(LangJa);
 
   const nlp = container.get('nlp');
 
   nlp.settings.autoSave = false;
 
   // add data manually i dont know how to do this
-  corpora.forEach(({ locale, data }) => {
+  corpora.forEach(({ locale, data }: corpus) => {
     nlp.addLanguage(locale);
 
-    data.forEach(({ intent, utterances, answers }) => {
-      utterances.forEach((utterance) => {
+    data.forEach(({ intent, utterances, answers }: data) => {
+      utterances.forEach((utterance: string) => {
         nlp.addDocument(locale, utterance, intent);
       });
 
-      answers.forEach((answer) => {
+      answers.forEach((answer: string) => {
         nlp.addAnswer(locale, intent, answer);
       });
     });
   });
 
   await nlp.train();
-  writeFileSync('./script/nlp/model.json', nlp.export(true));
+  writeFileSync(join(__dirname, 'model.json'), nlp.export(true));
 })();
